dc1a48d07b70ec937b71eb83cf094d75ab812efa
host1812 <host1812@gmail.com>
2022-01-31 17:18:50 -0800

    vm: just copy for now

---
+++ 06_meta_arguments/resource_groups.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "aftest-experimental" {
+  name     = "rg-aftest-experimental"
+  location = "West US 3"
+}
---
+++ 06_meta_arguments/virtual_networks.tf
@@ -0,0 +1,26 @@
+resource "azurerm_virtual_network" "aftest-vnet" {
+  name                = "aftest-vnet"
+  resource_group_name = azurerm_resource_group.aftest-experimental.name
+  location            = azurerm_resource_group.aftest-experimental.location
+
+  address_space = ["10.0.0.0/16"]
+  dns_servers   = ["10.0.0.4", "10.0.0.5"]
+
+  tags = {
+    "environment" = "staging"
+  }
+}
+
+resource "azurerm_subnet" "aftest-subnet1" {
+  name                 = "aftest-subnet1"
+  address_prefixes     = ["10.0.1.0/24"]
+  resource_group_name  = azurerm_resource_group.aftest-experimental.name
+  virtual_network_name = azurerm_virtual_network.aftest-vnet.name
+}
+
+resource "azurerm_subnet" "aftest-subnet2" {
+  name                 = "aftest-subnet2"
+  address_prefixes     = ["10.0.2.0/24"]
+  resource_group_name  = azurerm_resource_group.aftest-experimental.name
+  virtual_network_name = azurerm_virtual_network.aftest-vnet.name
+}
