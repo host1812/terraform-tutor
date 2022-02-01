8da54c6d21c336dfbabc4bfffbb9a04381393f9c
host1812 <host1812@gmail.com>
2022-02-01 15:26:24 -0800

    for_each: done

--- 08_for_each/resource_groups.tf
+++ 08_for_each/resource_groups.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "rg-aftest" {
-  name     = ""
-  location = "West US 3"
+  for_each = {
+    app1 = "eastus"
+    app2 = "eastus2"
+    app3 = "westus"
+  }
+  name     = "rg-${each.key}"
+  location = each.value
 }
