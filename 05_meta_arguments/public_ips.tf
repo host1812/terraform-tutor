766e8e96f7779c3bcf99f2c30e265f54d5e3f18f
host1812 <host1812@gmail.com>
2022-01-31 17:10:13 -0800

    remove all states

--- 05_meta_arguments/public_ips.tf
+++ 05_meta_arguments/public_ips.tf
@@ -1,9 +1,9 @@
 resource "azurerm_public_ip" "aftest-pip1" {
   # Testing deps
-  # depends_on = [
-  #   azurerm_virtual_network.aftest-vnet,
-  #   azurerm_subnet.aftest-subnet1
-  # ]
+  depends_on = [
+    azurerm_virtual_network.aftest-vnet,
+    azurerm_subnet.aftest-subnet1
+  ]
   name                = "aftest-pip1"
   resource_group_name = azurerm_resource_group.aftest-experimental.name
   location            = azurerm_resource_group.aftest-experimental.location
