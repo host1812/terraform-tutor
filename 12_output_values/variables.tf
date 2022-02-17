df9e3e18b329579de720eb7ea8ac0ffe443fb6ce
host1812 <host1812@gmail.com>
2022-02-17 15:47:34 -0800

    simple prestage

--- 12_output_values/variables.tf
+++ 12_output_values/variables.tf
@@ -22,39 +22,7 @@ variable "rgloc" {
   default     = "westus3"
 }
 
-variable "db_name" {
-  description = "DB Name"
-  type        = string
-}
-
-variable "db_admin_username" {
-  description = "DB Admin Username"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_admin_password" {
-  description = "DB Admin Password"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_size_mb" {
-  description = "DB Size (MB)"
-  type        = number
-}
-
-variable "db_auto_growth_enabled" {
-  description = "DB Auto Growth Enabled"
-  type        = bool
-}
-
-variable "db_thread_detection" {
-  description = "MySQL Thread Detection Policy"
-  type = object({
-    enabled              = bool
-    retention_days       = number
-    email_account_admins = bool
-    email_addresses      = list(string)
-  })
+variable "vnname" {
+  type    = string
+  default = "af"
 }
